f78cfc742010dd0678dce32579ec5a1290c54a4f
pc <svx327@naver.com>
2024-04-08 00:01:00 +0900

    Test : api 테스트 작성중

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../api";
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  const setInitData = async () => {
+    const data = await fetchCountries();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, []);
+
   return <div>Home</div>;
 };
 
